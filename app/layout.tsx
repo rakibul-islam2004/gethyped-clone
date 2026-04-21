761322aa7660c61162daed40f56799d54dca7f9d
rakibul-islam2004 <mdrakibulislamandrokib@gmail.com>
2026-04-21 14:13:54 +0600

    fix(footer): resolve responsive issue on Surface Duo

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
-      <body>
+      <body suppressHydrationWarning>
         <SmoothScroll />
         <Navbar />
         <main>
